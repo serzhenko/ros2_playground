b06ac9cc46e352322388bda93ae9aa34df63be0a
Dmitry Serzhenko <dmitry@serzhenko.ru>
2026-07-20 17:58:56 +0000

    lesson 3 - textbook

--- src/my_robot_controller/setup.py
+++ src/my_robot_controller/setup.py
@@ -24,8 +24,9 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'my_first_node = my_robot_controller.my_first_node:main'
-            
+            'my_first_node = my_robot_controller.my_first_node:main',
+            'robot_news_station = my_robot_controller.robot_news_station:main',
+            'smartphone = my_robot_controller.smartphone:main'
         ],
     },
 )
